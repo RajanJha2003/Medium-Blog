8d2b9b9e60e4035da3e73106fe16933d1be2425f
Rajan <rajanj448@student.sfit.ac.in>
2024-07-28 12:08:10 +0530

    Created sign-up route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,15 +2,33 @@ import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
-const app = new Hono()
+const app = new Hono<{
+	Bindings: {
+		DATABASE_URL: string
+	}
+}>();
 
-app.post('/api/v1/signup', (c) => {
+app.post('/api/v1/signup', async (c) => {
 
 
 
 const prisma = new PrismaClient({
-    datasourceUrl: env.DATABASE_URL,
-}).$extends(withAccelerate())
+    datasourceUrl: c.env.DATABASE_URL,
+}).$extends(withAccelerate());
+
+const body=await c.req.json();
+
+  try {
+    const user=await prisma.user.create({
+      data:{
+        email:body.email,
+        password:body.password
+      }
+    })
+    
+  } catch (error) {
+    
+  }
   return c.text('Signup Route')
 })
 
