396e4fa5b400fcf06796c6a1daa415d85f815bf0
Rajan <rajanj448@student.sfit.ac.in>
2024-08-03 15:52:33 +0530

    Middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,32 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
-import { sign } from "hono/jwt";
+import { sign, verify } from "hono/jwt";
 
 const app = new Hono<{
   Bindings: {
-    DATABASE_URL: string;
-    JWT_SECRET: string;
-  };
+    DATABASE_URL: string,
+    JWT_SECRET: string,
+  }
 }>();
 
-app.post("/api/v1/signup", async (c) => {
+
+app.use("/api/v1/blog/*",async(c,next)=>{
+  const header=c.req.header("authorization") || "";
+  const response=await verify(header,c.env.JWT_SECRET)
+  if(response.id){
+    next();
+  }else{
+    c.status(403);
+    return c.json({ error: "Unauthorised" });
+
+  }
+})
+
+app.post('/api/v1/signup', async (c) => {
+  // Ensure DATABASE_URL is directly passed
   const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
+    datasources: { db: { url: c.env.DATABASE_URL } },
   }).$extends(withAccelerate());
 
   const body = await c.req.json();
@@ -21,19 +35,57 @@ app.post("/api/v1/signup", async (c) => {
     const user = await prisma.user.create({
       data: {
         email: body.email,
-        password: body.password,
-      },
+        password: body.password
+      }
     });
 
-    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+    
 
+    if (!c.env.JWT_SECRET) {
+      throw new Error("JWT_SECRET is not set");
+    }
+
+    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
     return c.json({ jwt });
-  } catch (error) {}
-  return c.text("Signup Route");
+  } catch (e) {
+    
+    c.status(403);
+    return c.json({ error: "error while signing up" });
+  } 
 });
 
-app.post("/api/v1/signin", (c) => {
-  return c.text("Signin Route");
+app.post("/api/v1/signin", async (c) => {
+  const prisma = new PrismaClient({
+    datasources: { db: { url: c.env.DATABASE_URL } },
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: body.email
+      }
+    });
+
+    if (!user) {
+      c.status(403);
+      return c.json({ error: "user not found" });
+    }
+
+    console.log("JWT_SECRET:", c.env.JWT_SECRET);
+
+    if (!c.env.JWT_SECRET) {
+      throw new Error("JWT_SECRET is not set");
+    }
+
+    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+    return c.json({ jwt });
+  } catch (e) {
+    
+    c.status(403);
+    return c.json({ error: "error while signing in" });
+  } 
 });
 
 app.get("/api/v1/blog/:id", (c) => {
